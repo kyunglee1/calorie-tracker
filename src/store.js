6c64782ae19854d0144503be7c24cc9c5607e8a1
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-02-18 14:38:29 -0800

    Add Redux DevTools extension

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import localStorageMiddleware from './middleware/localStorageMiddleware';
 import rootReducer from './reducers/index';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(localStorageMiddleware, thunk)
+  composeWithDevTools(applyMiddleware(localStorageMiddleware, thunk))
 );
 
 export default store;
